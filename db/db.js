a62f241f7a756728a28ad953d86d55dd29c79296
Namratha L Bemane <42108024+Namrathalb@users.noreply.github.com>
2022-06-23 20:30:45 +0530

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,207 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Namratha L Bemane. I'm currently working as a Software Engineer at QuEST Global. Founder of Kodeverse Community. I did my Undergrad in CSE from NHCE Bangalore.",
+				"I've been a developer, Tester and currently as Build Manager in my present role. I am Passionate about building Products that solves real world challenges using technologies. I'm a Geek and Passionate about Building Products & Communities both in Tech and Non-Tech. I have experience of delivering solutions Software Builds and Leading a Team.",
+				"I love to Teach, listen to music, Network, Talk about Cypto, Web 3.0 and Pop culture.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a product, technology or Building a Coomunity or anything Pop-culture, a business, or to just say hi, feel free to send me an email at namratha2398@gmail.com",
+			],
+		},
+	},
+
+	experience: [
+		{
+			title: "QuEST Global",
+
+			duration: "September 2020 - Present",
+			subtitle: "Software Engineer - Build Manager",
+			details: [
+				"Responsible for Build Releases as part of Build Management Team of AT DIS (DC) Mainline Signalling -2.",
+				"Working towards fast delivery of software build releases as developer changes done, by performing integration testing and software build releaes for the Onboard teams",
+			],
+			tags: [ "Integration Testing", "Test scripts Automation", "Agile Methodolgies", "Scrum Master"],
+
+			subtitle: "Trainee Software Engineer",
+			details: [
+				"Acted as QA Tester for the ALSTOM DIS operations Tools team for ATE-OTS product. Designing Test cases, Black box Testing, Report documentation",
+			],
+			tags: ["QT - QML", "C++", "XAML", "Testcases & Reports"],
+
+			icon: "train",
+		},
+
+		{
+			title: "QuEST Global",
+			duration: "Jan 2020 - August 2020",
+			subtitle: "Project Intern",
+			details: [
+				"Used Raspberry pi to connect with Accelerometer and GPS module sensor attached to vehicle and remotely controlled the execution on roads to collect the data",
+			    "Published the Research paper in International Journal of Research in computer science, Engineering and Information Technology in Dec 2019.",
+			],
+			tags: ["Python", "Data Analytics", "Raspberry Pi", "Sensors"],
+			icon: "map-marked-alt",
+		},
+		{
+			title: "DOJO Sports Pvt Ltd",
+			duration: "Feb 2020 - May 2020",
+			subtitle: "Business Development Intern",
+			details: [
+				"Worked on Business development team by collecting valid data of sports achievers and handled their business by creating ideas, executing them through collaboration and improved Business portfolio",
+			],
+			tags: ["Social Media Portfolio", "MS Word", "MS Excel"],
+			icon: "business-time",
+		},
+		{
+			title: "WERP India",
+			duration: "Mar 2018 - May 2018",
+			subtitle: "Content Writer",
+			details: [
+				"Worked for social Media Team by creating posts for the Social website and collaborated with other NGOs for creating valuable content",
+			],
+			tags: ["Social Media Portfolio", "MS Word", "Website Content"],
+			icon: "edit",
+		},
+	],
+	community: [
+		{
+			title: "KodeVerse",
+			duration: "Mar 2021 - Present",
+			subtitle: "Community Founder",
+			details: [
+				"Founded small community to provide cold e-mail & profile building services for students and working professionals in product based companies.",
+			],
+			tags: ["Telegram", "LinkedIn", "Twitter"],			
+			icon: "network-wired",
+		},
+		{
+			title: "Microsoft Azure Developer India",
+			duration: "Sep 2021 - June 2022",
+			subtitle: "Chapter Lead - Tumkur",
+			details: [
+				"Lead a Tumkur region community on Azure deevloper tools and technology by hosting live online session on Git, Github, Gitlab, Blockchain and more by industry experts.",
+			],
+			tags: ["StreamYard", "Camva", "Meetups Page"],
+			icon: "cloud",
+		},
+		{
+			title: "GirlScript Foundation",
+			duration: "Aug 2019 - Aug 2021",
+			subtitle: "Community Organizer - Bangalore",
+			details: [
+				"collaborated with tech communities to organize Google dev fest 2019, Tensorflow roadshow with Microsoft reactor and more.",
+				"Co-Reviewed the Code merges by students for open source in Github branches for the monthly hosted coding challenges in chapter community Repository.",
+			],
+			tags: ["Social Media", "Github Code Reviewer", "Logistics"],
+			icon: "book-reader",
+		},
+		{
+			title: "Code to Express",
+			duration: "Aug 2019 - Aug 2021",
+			subtitle: "Community Volunteer",
+			details: [
+				"Handled and co-ordinated with teams in sharing all the notable meetups, tech events conferences and job updates to students and working professionals to grow network.",
+			],
+			tags: ["Social Media", "Community Building", "LinkedIn"],
+			icon: "code",
+		},
+	],
+	education: [
+		{
+			title: "B.Tech - Computer Science and Engineering",
+			duration: "Aug 2016 - Aug 2020",
+			subtitle: "New Horizon College of Engineering",
+			details: [
+				"Presented and Published a Paper on Safe Ride: Automatic Detection of Potholes and Humps on Roads using ultrasonic sensor in Iternational Conference on Innovation Research in Engineering, Management and Sciences (ICIREMS-2019).",
+				"Won 1st Prize at Social Innovation Hackathon Accelathon 2019 conducted by BCIC (Bangalore Chambers of Industry and Commerce) for the IoT Project SMART STICK.",
+				"Won 3rd prize at QuEST Global IoT Hackathon for the Web Development Project Digital Library.",
+				"Secured 9.44 aggregate CGPA in B.Tech"
+			],
+			tags: [
+				"Data Structures & Algorithms", "Computer Networks",
+				"Operating Systems", "Blockchain", "C/C++", "Internet of Things",
+				"DBMS (MySQL)", "Java",	"Web stack ( HTML, CSS, js)",	
+				"Compiler Designing", "Machine Learning", "Big Data",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "11th & 12th - Science",
+			duration: "June 2014 - May 2016",
+			subtitle: "Sri Adichunchanagiri Independent Pre University",
+			details: [
+				"Qualified JEE Mains.",
+				"Secured 91 percentile in Class 12th Boards Examinations.",
+			],
+			tags: ["Physics", "Chemistry", "Mathematics", "Biology",],
+			icon: "university",
+		},
+		{
+			title: "Mary Immaculate Girl's High School",
+			duration: "June 2011 - Mar 2014",
+			subtitle: "High School - Karnataka State Board",
+			details: [
+				"Secured 89.76 percentile in Class 10th Boards Examinations.",
+			],
+			tags: [ ],
+			icon: "school",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "GitHub",
+					link: "https://github.com/Namrathalb",
+				},				
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+				{
+					text: "Clone this page",
+					link: "https://github.com/Namrathalb/Namrathalb.github.io",
+				},
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/namratha-l-bemane/",
+				},
+				{
+					text: "Twitter",
+					link: "https://twitter.com/namratha_bemane",
+				},
+				{
+					text: "Instagram",
+					link: "https://www.instagram.com/namratha.bemane/",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"Made with &hearts; by Namratha L Bemane.",
+				"&copy; No Copyrights. Feel free to use this template."
+			],
+		},
+	],
+};
