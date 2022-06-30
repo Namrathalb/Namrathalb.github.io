ce986fda6a228d275979b579f5440284d7a37f33
Namratha L Bemane <42108024+Namrathalb@users.noreply.github.com>
2022-06-30 14:39:49 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -27,8 +27,7 @@ export default {
 	experience: [
 		{
 			title: "QuEST Global",
-			duration: "September 2020 - Present",
-			
+			duration: "December 2020 - Present",			
 			subtitle: "Software Engineer - Build Manager",
 			details: [
 				"Responsible for Build Releases as part of Build Management Team of AT DIS (DC) Mainline Signalling -2.",
@@ -36,6 +35,12 @@ export default {
 			],
 			tags: [ "Integration Testing", "Test scripts Automation", "Agile Methodolgies", "Scrum Master",],
 			
+			icon: "train",
+		},
+		
+		{
+			title: "QuEST Global",
+			duration: "September 2020 - December 2020",
 			subtitle: "Trainee Software Engineer",
 			details: [
 				"Acted as QA Tester for the ALSTOM DIS operations Tools team for ATE-OTS product. Designing Test cases, Black box Testing, Report documentation",
